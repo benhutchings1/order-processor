556f2a66b3eeeca6445b81ab91c1b9e406e12e49
Ben Hutchings <ben.hutchings>
2025-03-16 14:23:35 +0000

    feat: Order Processor

---
+++ OrderProcessor/Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderProcessor.Models;
+
+namespace OrderProcessor.Repository;
+
+public class OrderContext : DbContext
+{
+    public DbSet<OrderEntity> Order { get; set; } = null!;
+    public DbSet<OrderOutbox> Outbox { get; set; } = null!;
+
+    public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+}
